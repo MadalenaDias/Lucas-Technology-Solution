d1850d3a2914495cc2bf634c30b57151b6358c6c
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-11 02:33:25 +0000

    "feat(#285): implementando as entidades do microservice Service Desk"

---
+++ src/Modules/LucasTecnologiaServices.Modules.ServiceDesk/Models/Called.cs
@@ -0,0 +1,32 @@
+﻿using LucasTecnologiaServices.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.ServiceDesk.Models
+{
+    public class Called : EntityBase
+    {
+        
+        public Called() { }
+
+        public Called(long id)
+        {
+            Id = id;
+        }
+        
+        
+        public string OS { get; set; }
+
+        public DateTime OrigDate { get; set; }
+
+        public DateTime ClosedDate { get; set; }
+
+        public Category Category { get; set; }
+        public string CategoryId { get; set; }
+
+        public List<Category> Categories { get; set; }
+    }
+}
---
+++ src/Modules/LucasTecnologiaServices.Modules.ServiceDesk/Models/RoleServDesk.cs
@@ -0,0 +1,19 @@
+﻿using LucasTecnologiaServices.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.ServiceDesk.Models
+{
+    public class RoleServDesk : EntityBase
+    {
+        public RoleServDesk() { }
+
+        public RoleServDesk(long id)
+        {
+            Id = id;
+        }
+    }
+}
