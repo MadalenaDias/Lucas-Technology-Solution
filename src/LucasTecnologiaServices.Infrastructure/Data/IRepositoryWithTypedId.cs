1ee068d9bb0f4a9445dfb2ca6d56e3b971dd09ac
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-13 14:14:11 +0000

    "feat(#244): implementando as interfaces de Data "

---
+++ src/LucasTecnologiaServices.Infrastructure/Data/IRepositoryWithTypedId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Infrastructure.Data
+{
+    public interface IRepositoryWithTypedId<T, TId> where T : IEntityWithTypedId<TId>
+    {
+        IQueryable<T> Query();
+
+        void Add(T entity);
+
+        void AddRange(IEnumerable<T> entity);
+
+        IDbContextTransaction BeginTransaction();
+
+        void SaveChanges();
+
+        Task SaveChangesAsync();
+
+        void Remove(T entity);
+    }
+}
