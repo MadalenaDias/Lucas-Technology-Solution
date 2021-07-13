7a0cc06aceb15e37a58fb633f7153c99b2a25641
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-13 13:37:16 +0000

    "feat(#243):implementando classes de Localization no projeto de Infraestrutura"

---
+++ src/LucasTecnologiaServices.Infrastructure/Localization/Culture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Infrastructure.Localization
+{
+    public class Culture : EntityBaseWithTypedId<string>
+    {
+        public Culture(string id)
+        {
+            Id = id;
+        }
+        
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(450)]
+        public string Name { get; set; }
+
+        public IList<Resource> Resources { get; set; }
+    }
+}
