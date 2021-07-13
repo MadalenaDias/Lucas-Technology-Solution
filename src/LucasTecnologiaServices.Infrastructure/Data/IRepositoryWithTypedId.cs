c6db0ca7203a8ad6860e27ef155b56ff92a289ea
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-13 21:11:44 +0000

    resolvendo erros de pendencias

--- src/LucasTecnologiaServices.Infrastructure/Data/IRepositoryWithTypedId.cs
+++ src/LucasTecnologiaServices.Infrastructure/Data/IRepositoryWithTypedId.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LucasTecnologiaServices.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
