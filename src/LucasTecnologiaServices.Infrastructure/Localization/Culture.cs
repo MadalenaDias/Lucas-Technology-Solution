650cea779c7480eef3160d469370ab06a4cee890
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-13 20:52:33 +0000

    Revert ""feat(#243): resolvendo erros de dependencia""
    This reverts commit e0d99e6f5e54a7f69589869e105b7059872ebb83.

--- src/LucasTecnologiaServices.Infrastructure/Localization/Culture.cs
+++ src/LucasTecnologiaServices.Infrastructure/Localization/Culture.cs
@@ -1,7 +1,5 @@
-﻿using LucasTecnologiaServices.Infrastructure.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
