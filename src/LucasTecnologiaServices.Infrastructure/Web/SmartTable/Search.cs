383939cd20907e77b3313a8ba1684e9984877758
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-15 18:39:46 +0000

    feat(#246):implementando as classes do módulo Web

---
+++ src/LucasTecnologiaServices.Infrastructure/Web/SmartTable/Search.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Infrastructure.Web.SmartTable
+{
+    public class Search
+    {
+        public JObject PredicateObject { get; set; }
+    }
+}
