ea99ea537977878bc1675a24320bbdfbe068767e
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-15 01:36:32 +0000

    feat(#257): implementando as classes de data e classes de models do Microservice Core

---
+++ src/Modules/LucasTecnologiaServices.Modules.Core/Data/CoreCustomModelBuilder.cs
@@ -0,0 +1,21 @@
+﻿using LucasTecnologiaServices.Infrastructure.Data;
+using LucasTecnologiaServices.Modules.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Core.Data
+{
+    public class CoreCustomModelBuilder : ICustomModelBuilder
+    {
+        public void Build(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppSetting>().ToTable("Core_AppSetting");
+            modelBuilder.Entity<User>()
+                .ToTable("Core_User");
+        }
+    }
+}
