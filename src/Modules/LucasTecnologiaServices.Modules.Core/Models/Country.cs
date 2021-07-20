61458aec719e9b880eeb61c0f56d6296f15cea2c
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-20 20:14:17 +0000

    feat(#300): implementando as clqasses de modls dos microservicos Core e Tax

--- src/Modules/LucasTecnologiaServices.Modules.Core/Models/Country.cs
+++ src/Modules/LucasTecnologiaServices.Modules.Core/Models/Country.cs
@@ -1,3 +1,4 @@
+using LucasTecnologiaServices.Infrastructure.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -7,8 +8,31 @@ using System.Threading.Tasks;
 
 namespace LucasTecnologiaServices.Modules.Core.Models
 {
-    public class Country
+    public class Country : EntityBase
     {
-        
+        public Country(string id)
+        {
+            Id = id;
+        }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(450)]
+        public string Name { get; set; }
+
+        [StringLength(450)]
+        public string Code3 { get; set; }
+
+        public bool IsBillingEnabled { get; set; }
+
+        public bool IsShippingEnabled { get; set; }
+
+        public bool IsCityEnabled { get; set; } = true;
+
+        public bool IsZipCodeEnabled { get; set; } = true;
+
+        public bool IsDistrictEnabled { get; set; } = true;
+
+        public IList<StateOrProvince> StatesOrProvinces { get; set; } = new List<StateOrProvince>();
+
     }
 }
