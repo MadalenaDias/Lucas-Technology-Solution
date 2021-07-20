b6c7ab21820a78d291284e6c1d79a7f39233f084
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-20 19:57:29 +0000

    feat(#277):implementando a classe de Services

---
+++ src/Modules/LucasTecnologiaServices.Modules.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<IList<CategoryListItem>> GetAll();
+
+        Task Create(Category category);
+
+        Task Update(Category category);
+
+        Task Delete(Category category);
+    }
+}
---
+++ src/Modules/LucasTecnologiaServices.Modules.Catalog/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Catalog.Services
+{
+    public interface IProductService
+    {
+        void Create(Product product);
+
+        void Update(Product product);
+
+        Task Delete(Product product);
+    }
+}
