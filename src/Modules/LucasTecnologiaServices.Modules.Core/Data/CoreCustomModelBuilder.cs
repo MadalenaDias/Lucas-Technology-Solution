1aae074e54e15d313fff196f1c9c2abc0c0d582c
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-22 16:39:06 +0000

    feat(#257): implementando modulo Data do microservico Core

--- src/Modules/LucasTecnologiaServices.Modules.Core/Data/CoreCustomModelBuilder.cs
+++ src/Modules/LucasTecnologiaServices.Modules.Core/Data/CoreCustomModelBuilder.cs
@@ -1,10 +1,14 @@
 using LucasTecnologiaServices.Infrastructure.Data;
 using LucasTecnologiaServices.Modules.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LucasTecnologiaServices.Modules.Core.Data
 {
     public class CoreCustomModelBuilder : ICustomModelBuilder
     {
-        
+        public void Build(ModelBuilder modelBuilder)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
