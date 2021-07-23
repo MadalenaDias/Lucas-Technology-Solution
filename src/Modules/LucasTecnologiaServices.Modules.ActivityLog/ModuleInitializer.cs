a4bef9a625af914b4b5bf98aade1ff03947f511e
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-23 18:19:38 +0000

    feat(#308):implementando arquivos json

---
+++ src/Modules/LucasTecnologiaServices.Modules.ActivityLog/ModuleInitializer.cs
@@ -0,0 +1,31 @@
+﻿using LucasTecnologiaServices.Infrastructure;
+using LucasTecnologiaServices.Infrastructure.Modules;
+using LucasTecnologiaServices.Modules.ActivityLog.Data;
+using LucasTecnologiaServices.Modules.ActivityLog.Events;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.ActivityLog
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IActivityTypeRepository, ActivityRepository>();
+            services.AddTransient<INotificationHandler<EntityViewed>, EntityViewedHandler>();
+
+            GlobalConfiguration.RegisterAngularModule("simplAdmin.activityLog");
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
