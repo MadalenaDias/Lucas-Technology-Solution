3301bb156de0f1ed714f751088335298c5df7e0b
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-25 14:17:55 +0000

    Updating

---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp/Fts/BulStem/StemmingLevel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace LucasTecnologiaServices.Modules.Erp.Fts.BulStem
+{
+    public class StemmingLevel
+    {
+        
+    }
+}
---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp/Fts/FtsAnalyzer.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace LucasTecnologiaServices.Modules.Erp.Fts
+{
+    public class FtsAnalyzer
+    {
+        
+    }
+}
