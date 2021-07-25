a40efd7a841dc56d29a18393a34b1ef9f285973d
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-07-25 20:42:36 +0000

    Implementando a estrutura de APIs do microservi;o Core

---
+++ src/Modules/LucasTecnologiaServices.Modules.Core/Areas/Core/ViewModels/Manage/UserSettingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Core.Areas.ViewModels.Manage
+{
+    public class UserSettingViewModel
+    {
+    }
+}
