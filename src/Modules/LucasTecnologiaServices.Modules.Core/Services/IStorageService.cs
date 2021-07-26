bb2619dda127f67c747b11ac882937e2da2b1eba
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-26 18:32:18 +0000

    feat(#256):implementando interfaces e classes de services no projeto de microserviço core

---
+++ src/Modules/LucasTecnologiaServices.Modules.Core/Services/IStorageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Core.Services
+{
+    public interface IStorageService
+    {
+        string GetMediaUrl(string fileName);
+
+        Task SaveMediaAsync(Stream mediaBinaryStream, string fileName, string mimeType = null);
+
+        Task DeleteMediaAsync(string fileName);
+    }
+}
