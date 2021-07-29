530bcd258e044a4cb96e678dffca12e03e1667f8
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-29 17:21:05 +0000

    feat(#341): Implementando as classes de Exceptions no projeto de microserviço de Erp

--- src/Modules/ERP/LucasTecnologiaServices.Modules.Erp/Exceptions/StorageException.cs
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp/Exceptions/StorageException.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace LucasTecnologiaServices.Modules.Erp.Exceptions
 {
-    public class StorageException
+    public class StorageException : Exception
     {
+        public StorageException(Exception innerException = null)
+           : base(innerException.Message, innerException)
+        {
+        }
+
+        public StorageException(string message = null, Exception innerException = null)
+            : base(message, innerException)
+        {
+        }
     }
 }
