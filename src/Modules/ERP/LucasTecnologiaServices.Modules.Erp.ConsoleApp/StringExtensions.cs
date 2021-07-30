d8074c14ea33993b841f83e3b54a98d9ea3dd073
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-07-30 17:06:26 +0000

    feat(#350):Implementando microserviço Console App dentro da coleção Erp

---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.ConsoleApp/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.ConsoleApp
+{
+	public static class StringExtensions
+	{
+		//utility method to get configuration file path
+		public static string ToApplicationPath(this string fileName)
+		{
+			var exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+			Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+			var appRoot = appPathMatcher.Match(exePath).Value;
+			return Path.Combine(appRoot, fileName);
+		}
+	}
+}
