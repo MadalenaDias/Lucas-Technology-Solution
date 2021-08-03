1c864fefd152d5e8c6976fd752021fde8f7a5288
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-03 17:07:07 +0000

    feat(#358):Implementando ViewModels do microserviço Contacts

---
+++ src/Modules/LucasTecnologiaServices.Modules.Contacts/Areas/Contacts/ViewModels/ContactAreaForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Contacts.Areas.Contacts.ViewModels
+{
+    public class ContactAreaForm
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        public string Name { get; set; }
+    }
+}
