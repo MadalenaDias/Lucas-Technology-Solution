587726c1e58836611aec0b6abd66e163e9b41ee3
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-03 18:24:51 +0000

    feat(#362):Implementando Services no microserviço Inventory

---
+++ src/Modules/LucastecnologiaServices.Modules.Inventory/Services/IStockService.cs
@@ -0,0 +1,16 @@
+﻿using LucastecnologiaServices.Modules.Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucastecnologiaServices.Modules.Inventory.Services
+{
+    public interface IStockService
+    {
+        Task AddAllProduct(Warehouse warehouse);
+
+        Task UpdateStock(StockUpdateRequest stockUpdateRequest);
+    }
+}
---
+++ src/Modules/LucastecnologiaServices.Modules.Inventory/Services/StockUpdateRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucastecnologiaServices.Modules.Inventory.Services
+{
+    public class StockUpdateRequest
+    {
+        public long UserId { get; set; }
+
+        public long ProductId { get; set; }
+
+        public long WarehouseId { get; set; }
+
+        public int AdjustedQuantity { get; set; }
+
+        public string Note { get; set; }
+    }
+}
