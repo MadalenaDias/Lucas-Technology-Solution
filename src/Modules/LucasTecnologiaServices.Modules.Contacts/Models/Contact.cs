a32cd1414f7d2e89717201e0c91700dc0232c4cf
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-03 13:42:17 +0000

    feat(#355):Implementando as Models no microserviço Contacts

---
+++ src/Modules/LucasTecnologiaServices.Modules.Contacts/Models/Contact.cs
@@ -0,0 +1,40 @@
+﻿using LucasTecnologiaServices.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Contacts.Models
+{
+    public class Contact : EntityBase
+    {
+        public Contact()
+        {
+            CreatedOn = DateTimeOffset.Now;
+        }
+
+        [StringLength(450)]
+        public string FullName { get; set; }
+
+        [StringLength(450)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(450)]
+        public string EmailAddress { get; set; }
+
+        [StringLength(450)]
+        public string Address { get; set; }
+
+        public string Content { get; set; }
+
+        public long ContactAreaId { get; set; }
+
+        public virtual ContactArea ContactArea { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
