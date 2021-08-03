056e977bf058913e2dd667c19bd54f545a6fc9cd
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-03 17:33:57 +0000

    feat(#358):Implementando ViewModels no microserviço Contacts

---
+++ src/Modules/LucasTecnologiaServices.Modules.Contacts/ModuleInitializer.cs
@@ -0,0 +1,25 @@
+﻿using LucasTecnologiaServices.Infrastructure.Modules;
+using LucasTecnologiaServices.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Contacts
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            GlobalConfiguration.RegisterAngularModule("simplAdmin.contacts");
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
