39c4de027229ff43755a6ddded3a67b828a78ed7
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-03 18:35:56 +0000

    feat(#363):Implementando ViewModels no microserviço Inventory

---
+++ src/Modules/LucastecnologiaServices.Modules.Inventory/Areas/Inventory/ViewModels/StockVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucastecnologiaServices.Modules.Inventory.Areas.Inventory.ViewModels
+{
+    public class StockVm
+    {
+        public long ProductId { get; set; }
+
+        public int AdjustedQuantity { get; set; }
+
+        public string Note { get; set; }
+    }
+}
