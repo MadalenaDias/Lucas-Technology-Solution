2038286fe7b4f50f2fe5a2e8bab82bc5380bde1e
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-05 17:09:59 +0000

    feat(#375):Implementando o modulo Data no microserviço Tax

---
+++ src/Modules/LucasTecnologiaServices.Modules.Tax/Areas/Data/TaxCustomModelBuilder.cs
@@ -0,0 +1,24 @@
+﻿using LucasTecnologiaServices.Infrastructure.Data;
+using LucasTecnologiaServices.Modules.Core.Models;
+using LucasTecnologiaServices.Modules.Tax.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Tax.Areas.Data
+{
+    public class TaxCustomModelBuilder : ICustomModelBuilder
+    {
+        public void Build(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaxClass>().HasData(new TaxClass(1) { Name = "Standard VAT" });
+
+            modelBuilder.Entity<AppSetting>().HasData(
+                new AppSetting("Tax.DefaultTaxClassId") { Module = "Tax", IsVisibleInCommonSettingPage = true, Value = "1" }
+            );
+        }
+    }
+}
