255c7e9a7ef8a64f1e5415915f5df164b69b0f83
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-05 12:42:51 +0000

    feat(#371):Implementando os Services no microserviço LucasTecnologiaServices.Modules.Erp.Web

---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.Web/Services/WebSettingsService.cs
@@ -0,0 +1,19 @@
+﻿using LucasTecnologiaServices.Modules.Erp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.Web.Services
+{
+	public class WebSettingsService : BaseService
+	{
+		WebSettings coresettings = new WebSettings();
+
+		public WebSettings Get()
+		{
+			return coresettings;
+		}
+	}
+}
