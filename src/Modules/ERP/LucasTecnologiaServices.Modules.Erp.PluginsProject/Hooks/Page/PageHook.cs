97915771e6340766bca268df5e6a57c6adacd9c9
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-06 17:46:48 +0000

    feat(#387):Implementando Hooks no microserviço LucasTecnologiaServices.Modules.Erp.PluginsProject

---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.PluginsProject/Hooks/Page/PageHook.cs
@@ -0,0 +1,44 @@
+﻿using LucasTecnologiaServices.Modules.Erp.PluginsProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.PluginsProject.Hooks.Page
+{
+	[HookAttachment]
+	public class PageHook : IPageHook
+	{
+		private Guid CREATE_TASK_PAGE_ID = new Guid("68100014-1fd7-456c-9b26-27aa9f858287");
+
+		public IActionResult OnGet(BaseErpPageModel pageModel)
+		{
+			var page = pageModel.ErpRequestContext.Page;
+
+			// Init task create form
+			if (page != null && page.Id == CREATE_TASK_PAGE_ID)
+			{
+				EntityRecord record = (EntityRecord)pageModel.DataModel.GetProperty("Record");
+
+				var resultRecord = new TaskService().GetPageHookLogic(pageModel, record);
+
+				pageModel.DataModel.SetRecord(resultRecord);
+			}
+
+			//User menu items
+			new RenderService().UserMenuItemsManagement(pageModel);
+
+			return null;
+		}
+
+		public IActionResult OnPost(BaseErpPageModel pageModel)
+		{
+			//User menu items
+			new RenderService().UserMenuItemsManagement(pageModel);
+
+			return null;
+		}
+
+	}
+}
---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.PluginsProject/Hooks/Page/SetTaskRecurrence.cs
@@ -0,0 +1,18 @@
+﻿using LucasTecnologiaServices.Modules.Erp.PluginsProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.PluginsProject.Hooks.Page
+{
+	[HookAttachment(key: "SetTaskRecurrence")]
+	public class SetTaskRecurrence : IRecordDetailsPageHook
+	{
+		public IActionResult OnPost(RecordDetailsPageModel pageModel)
+		{
+			return new TaskService().SetTaskRecurrenceOnPost(pageModel);
+		}
+	}
+}
