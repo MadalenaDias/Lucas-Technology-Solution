4608c0d9b6e3682ea83e5a81b0facc0cbfa2aa3b
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-06 12:33:31 +0000

    feat(#3771):Implementando Extensions no microserviço Core

---
+++ src/Modules/LucasTecnologiaServices.Modules.Core/Extensions/SimplRoleStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Core.Extensions
+{
+    public class SimplRoleStore : RoleStore<Role, SimplDbContext, long, UserRole, IdentityRoleClaim<long>>
+    {
+        public SimplRoleStore(SimplDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/Modules/LucasTecnologiaServices.Modules.Core/Extensions/SimplUserStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Core.Extensions
+{
+    public class SimplUserStore : UserStore<User, Role, SimplDbContext, long, IdentityUserClaim<long>, UserRole,
+        IdentityUserLogin<long>, IdentityUserToken<long>, IdentityRoleClaim<long>>
+    {
+        public SimplUserStore(SimplDbContext context, IdentityErrorDescriber describer) : base(context, describer)
+        {
+        }
+    }
+}
