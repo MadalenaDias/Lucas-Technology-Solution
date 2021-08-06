ec352200fcf9755af21efeedd92a68a9f73c9ad5
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-06 19:24:37 +0000

    feat(#391):Implementando Model no microserviço LucasTecnologiaServices.Modules.Erp.Plugins.Sdk

---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.Plugin.Sdk/Model/AreaSelectionTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.Plugin.Sdk.Model
+{
+	public class AreaSelectionTree
+	{
+		[JsonProperty(PropertyName = "area_id")]
+		public Guid AreaId { get; set; } = Guid.Empty;
+
+		[JsonProperty(PropertyName = "all_nodes")]
+		public List<SelectOption> AllNodes { get; set; } = new List<SelectOption>();
+	}
+}
