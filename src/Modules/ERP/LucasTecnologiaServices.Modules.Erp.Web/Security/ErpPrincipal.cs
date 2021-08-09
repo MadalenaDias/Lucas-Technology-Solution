c3d382a3c9454636cb0bdb1788e6fa4d127ec951
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-09 19:29:29 +0000

    feat(#405):Implementando Security no microserviço LucasTecnologiaServices.Modules.ERP.Web

---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.Web/Security/ErpPrincipal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.Web.Security
+{
+    public class ErpPrincipal : ClaimsPrincipal
+//    {
+//        public ErpPrincipal(ErpIdentity identity) : base()
+//        {
+//            AddIdentity(identity);
+//        }
+//    }
+}
---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.Web/Security/HttpUnauthorizedResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.Web.Security
+{
+    public class HttpUnauthorizedResult : StatusCodeResult
+//    {
+//        public HttpUnauthorizedResult()
+//            : this(null)
+//        {
+//        }
+
+    //        public HttpUnauthorizedResult(string statusDescription)
+    //            : base((int)HttpStatusCode.Unauthorized)
+    //        {
+    //        }
+    //    }
+}
