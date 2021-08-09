10e711e013640955c5793d99663ff0b823d9939b
Nadja Cerioli <comercial2018@lucastecnologiaservices.onmicrosoft.com>
2021-08-09 16:30:57 +0000

    feat(#400):Implementando Models no microserviço LucasTecnologiaServices.Modules.ERP.Web

---
+++ src/Modules/ERP/LucasTecnologiaServices.Modules.Erp.Web/Models/Automapper/Profiles/ValidationErrorProfile.cs
@@ -0,0 +1,26 @@
+﻿using LucasTecnologiaServices.Modules.Erp.Api.Models;
+using LucasTecnologiaServices.Modules.Erp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTecnologiaServices.Modules.Erp.Web.Models.Automapper.Profiles
+{
+	public class ValidationErrorProfile : Profile
+	{
+		public ValidationErrorProfile()
+		{
+			CreateMap<ErrorModel, ValidationError>().ConvertUsing(source => JTokenToAppConvert(source));
+		}
+
+		private static ValidationError JTokenToAppConvert(ErrorModel data)
+		{
+			if (data == null)
+				return null;
+
+			return new ValidationError(data.Key, data.Message);
+		}
+	}
+}
