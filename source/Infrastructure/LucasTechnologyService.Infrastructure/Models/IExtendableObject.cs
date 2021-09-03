a9b99a2bc8203f2c6ad7d3618bfec69a4fddc45f
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-09-03 00:27:24 +0000

    fet(#498): implementando as classes do microservico Infrastructure

--- source/Infrastructure/LucasTechnologyService.Infrastructure/Models/IExtendableObject.cs
+++ source/Infrastructure/LucasTechnologyService.Infrastructure/Models/IExtendableObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace LucasTechnologyService.Infrastructure.Models
     public interface IExtendableObject
     {
         string ExtensionData { get; set; }
+                
     }
 }
--- source/Modules/LucasTechnologyServices.Module.Core/Models/Entity.cs
+++ source/Modules/LucasTechnologyServices.Module.Core/Models/Entity.cs
@@ -1,18 +1,11 @@
 using System;
 namespace LucasTechnologyServices.Module.Core.Models
 {
-    public abstract class Entity
+    public class Entity
     {
 
 
-        public Entity(Parameters)
-        {
-            Id = Guid.NewGuid();
-            
-        }
         
-        
-        public Guid Id { get; private set; }
         
     }
 }
