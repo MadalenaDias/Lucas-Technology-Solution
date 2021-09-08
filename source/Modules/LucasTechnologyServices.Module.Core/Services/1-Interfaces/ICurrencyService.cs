55e4d959114438425f589e58d9cb6b955850c74a
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-09-08 00:20:02 +0000

    feat(#501): implementing Service interfaces into Core microservoce

---
+++ source/Modules/LucasTechnologyServices.Module.Core/Services/1-Interfaces/ICurrencyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTechnologyServices.Module.Core.Services._1_Interfaces
+{
+    public interface ICurrencyService
+    {
+        CultureInfo CurrencyCulture { get; }
+        string FormatCurrency(decimal value);
+    }
+}
---
+++ source/Modules/LucasTechnologyServices.Module.Core/Services/1-Interfaces/IMediaService.cs
@@ -0,0 +1,24 @@
+﻿using LucasTechnologyServices.Module.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTechnologyServices.Module.Core.Services._1_Interfaces
+{
+    public interface IMediaService
+    {
+        string GetMediaUrl(Media media);
+
+        string GetMediaUrl(string fileName);
+
+        string GetThumbnailUrl(Media media);
+
+        Task SaveMediaAsync(Stream mediaBinaryStream, string fileName, string mimeType = null);
+
+        Task DeleteMediaAsync(Media media);
+
+        Task DeleteMediaAsync(string fileName);
+    }
+}
