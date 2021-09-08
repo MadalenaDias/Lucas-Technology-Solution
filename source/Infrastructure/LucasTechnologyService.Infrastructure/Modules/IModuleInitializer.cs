915696a7f19e9ebf3944484137dca6b494ccaa4c
unknown <madalena.dias@lucastecnologiaservices.onmicrosoft.com>
2021-09-08 22:34:02 +0000

    feat(#506): implementing module classes in Infrastructure Project

---
+++ source/Infrastructure/LucasTechnologyService.Infrastructure/Modules/IModuleInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTechnologyService.Infrastructure.Modules
+{
+    public interface IModuleInitializer
+    {
+        void ConfigureServices(IServiceCollection serviceCollection);
+
+        void Configure(IApplicationBuilder app, IWebHostEnvironment env);
+    }
+}
---
+++ source/Infrastructure/LucasTechnologyService.Infrastructure/Modules/ModuleInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTechnologyService.Infrastructure.Modules
+{
+    public class ModuleInfo
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public bool IsBundledWithHost { get; private set; }
+
+        public Version Version { get; private set; }
+
+        public Assembly Assembly { get; private set; }
+    }
+}
