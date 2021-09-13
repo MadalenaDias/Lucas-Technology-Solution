8eaf3e7d28401622690ad72b53726658f9b80c76
Jonas JS. Shurts <jonas.shurts@lucastecnologiaservices.com.br>
2021-09-13 21:17:24 +0000

    feat(#515): implementing models into Crm microservice

---
+++ source/Modules/LucasTechnologyServices.Module.Crm/Models/Page.cs
@@ -0,0 +1,14 @@
+﻿using LucasTechnologyServices.Module.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTechnologyServices.Module.Crm.Models
+{
+    public class Page : Content
+    {
+        public string Body { get; private set; }
+    }
+}
