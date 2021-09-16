5f03a8a6a957e95e4ce978053699b8a5a946543c
Jonas JS. Shurts <jonas.shurts@outlook.com>
2021-09-16 22:53:47 +0000

    feat(#518): implementing web Web classes

--- source/Infrastructure/LucasTechnologyService.Infrastructure/Web/HttpRequestExtentions.cs
+++ source/Infrastructure/LucasTechnologyService.Infrastructure/Web/HttpRequestExtentions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace LucasTechnologyService.Infrastructure.Web
 {
-    public class HttpRequestExtentions
+    public static class HttpRequestExtentions
     {
+        public static string GetFullHostingUrlRoot(this HttpRequest request)
+        {
+            return $"{request.Scheme}://{request.Host.Value}";
+        }
     }
 }
