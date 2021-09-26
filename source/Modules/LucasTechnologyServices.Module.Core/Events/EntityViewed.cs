72948739c83349c160dcf13d121f8819f0a081ba
Lester Cerioli <lesterlucasit@hotmail.com>
2021-09-26 14:31:12 +0000

    feat(#537): implementando as classes de Eventos

---
+++ source/Modules/LucasTechnologyServices.Module.Core/Events/EntityViewed.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasTechnologyServices.Module.Core.Events
+{
+    public class EntityViewed : INotification
+    {
+        public long EntityId { get; set; }
+        public string EntityTypeId { get; set; }
+    }
+}
