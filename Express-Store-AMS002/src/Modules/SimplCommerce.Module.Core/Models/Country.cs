fa7b6fe69f6924ec7b1fe14d3d8d6cc6faee8c40
Frederico Teles <frederico.silvateles@outlook.com>
2021-10-02 00:33:41 +0000

    feat(#549): implementing MVC identity~

---
+++ Express-Store-AMS002/src/Modules/SimplCommerce.Module.Core/Models/Country.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using SimplCommerce.Infrastructure.Models;
+
+namespace SimplCommerce.Module.Core.Models
+{
+    public class Country : EntityBase
+    {
+        public string Name { get; set; }
+
+        public string Code2 { get; set; }
+
+        public string Code3 { get; set; }
+
+        public bool IsBillingEnabled { get; set; }
+
+        public bool IsShippingEnabled { get; set; }
+
+        public bool IsCityEnabled { get; set; } = true;
+
+        public bool IsPostalCodeEnabled { get; set; } = true;
+
+        public bool IsDistrictEnabled { get; set; } = true;
+
+        public IList<StateOrProvince> StatesOrProvinces { get; set; } = new List<StateOrProvince>();
+
+    }
+}
---
+++ Express-Store-AMS002/src/Modules/SimplCommerce.Module.News/Data/NewsCustomModelBuilder.cs
@@ -0,0 +1,20 @@
+﻿using SimplCommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using SimplCommerce.Module.News.Models;
+
+namespace SimplCommerce.Module.News.Data
+{
+    public class NewsCustomModelBuilder : ICustomModelBuilder
+    {
+        public void Build(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NewsItemCategory>(b =>
+            {
+                b.HasKey(ur => new { ur.CategoryId, ur.NewsItemId });
+                b.HasOne(ur => ur.Category).WithMany(r => r.NewsItems).HasForeignKey(r => r.CategoryId).OnDelete(DeleteBehavior.Cascade);
+                b.HasOne(ur => ur.NewsItem).WithMany(u => u.Categories).HasForeignKey(u => u.NewsItemId).OnDelete(DeleteBehavior.Cascade);
+                b.ToTable("News_NewsItemCategory");
+            });
+        }
+    }
+}
