9483cdf75a341016327dda2dc37ef49400756f8e
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-10-19 22:17:42 +0000

    removing projects

--- ALOS Solution/source/Modules/Core/ALOS.Module.Core/backoffice/models/ageing/ageingSlab.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AgeingSlab = void 0;
-class AgeingSlab {
-}
-exports.AgeingSlab = AgeingSlab;
-//# sourceMappingURL=ageingSlab.model.js.map
--- ALOS Solution/source/Modules/Core/ALOS.Module.Core/backoffice/models/attachment/attachmentType.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AttachmentType = void 0;
-class AttachmentType {
-}
-exports.AttachmentType = AttachmentType;
-//# sourceMappingURL=attachmentType.model.js.map
