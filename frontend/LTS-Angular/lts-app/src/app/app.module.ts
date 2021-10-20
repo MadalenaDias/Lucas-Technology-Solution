a910a4d9dff3d57598f76c868f60eddf799a7872
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-10-20 03:23:31 +0000

    Updated app.module.ts

--- frontend/LTS-Angular/lts-app/src/app/app.module.ts
+++ frontend/LTS-Angular/lts-app/src/app/app.module.ts
@@ -6,12 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { InvoiceControlComponent } from './finance/invoice-control/invoice-control.component';
+import { PaymentSchedulingComponent} from './finance/payment-scheduling/payment-scheduling.component'
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  bootstrap: [
+      AppComponent,
+      InvoiceControlComponent,
+      PaymentSchedulingComponent
+
+  ],
 })
 export class AppModule {}
