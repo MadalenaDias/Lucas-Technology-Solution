ca44f17d37af7ea6b7ba88f6b367eca47e8594a0
Madalena MD. Dias <madalena.dias@lucastecnologiaservices.com.br>
2021-10-21 12:48:57 +0000

    fet(#565): implementing MundBlazor to style components and style navigation

--- frontend/LTS-Blazor/LTS.BLZ/LTS.Erp/Program.cs
+++ frontend/LTS-Blazor/LTS.BLZ/LTS.Erp/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using MudBlazor.Services;
 
 namespace LTS.Erp
 {
@@ -17,7 +18,9 @@ namespace LTS.Erp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
