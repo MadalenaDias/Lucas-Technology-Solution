49e281ef679b5fb50ed8f5cfe75170ffe7a326fd
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-10-23 02:31:15 +0000

    fet(#575): implementing unit tests

---
+++ frontend/LTS-Tripe/tests/LTS.Erp.Server.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace LTS.Erp.Server.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ frontend/LTS-Tripe/tests/LTS.Erp.Shared.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace LTS.Erp.Shared.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
