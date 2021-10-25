9b16ee60166cc0ec604a49796f007c4eede68500
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-10-25 00:58:26 +0000

    Deleted ERP-DMS001

--- ERP-DMS001/WebVella.Erp/Api/Models/SearchResultList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WebVella.Erp.Api.Models
-{
-	public class SearchResultList : List<SearchResult>
-	{
-		public int TotalCount { get; set; } = 0;
-	}
-}
