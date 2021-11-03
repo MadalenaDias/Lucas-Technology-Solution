cac65cbd731df015620cac48530587978c98bcde
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-11-03 23:49:02 +0000

    "fet(#601): adjusting Service Desk data module"

--- source/Modules/LucasTechnologyServices.Module.ServiceDesk/Data/ServiceDeskCustomModelBuilder.cs
+++ source/Modules/LucasTechnologyServices.Module.ServiceDesk/Data/ServiceDeskCustomModelBuilder.cs
@@ -1,10 +1,13 @@
+using LucasTechnologyService.Infrastructure.Data.Repositories.Common.EFCore;
 using System;
-using Microsoft.EntityFrameworkCore;
 
 namespace LucasTechnologyServices.Module.ServiceDesk.Data
 {
     public class ServiceDeskCustomModelBuilder : ICustomModelBuilder
     {
-        
+        public void build(ICustomModelBuilder modelBuilder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
