06012123464565fdbb94bf4e53589f29a1029bb3
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-11-03 02:40:49 +0000

    fet(#601): implementing Models classes and data model class into Service Desk microservice

--- source/Modules/LucasTechnologyServices.Module.ServiceDesk/Models/CategoryCall.cs
+++ source/Modules/LucasTechnologyServices.Module.ServiceDesk/Models/CategoryCall.cs
@@ -1,56 +1,55 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using LucasTechnologyService.Infrastructure.Models;
+using LucasTechnologyServices.Module.Core.Models;
+using System.Collections.Generic;
 
 namespace LucasTechnologyServices.Module.ServiceDesk.Models
 {
     public class CategoryCall : EntityBase
     {
         public string Name { get; set; }
-        
+
         public string MetaTitle { get; set; }
-        
+
         public string KeyWords { get; set; }
-        
+
         public string MetaDescription { get; set; }
-        
+
         public string Description { get; set; }
-        
+
         public int DisplayCall { get; set; }
-        
+
         public bool IsPublished { get; set; }
-        
+
         public bool IsDeleted { get; set; }
-        
+
         public bool IncludeinMenu { get; set; }
-        
+
         public long? ParentId { get; set; }
 
-        public Category Parent { get; set; }
-        
-        public IList<Category> Children { get; protected set; } = new List<Category>();
+        public CategoryCall Parent { get; set; }
+
+        public IList<CategoryCall> Children { get; protected set; } = new List<CategoryCall>();
 
         public Media ThumbnailImage { get; set; }
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
