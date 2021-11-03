29bc74ef8e5cf04c4bd8694755fb68c3a9986064
ChristianSchimidt <ChristianSchimidft>
2021-11-03 02:23:19 +0000

    fet(#601): implementing Models Entities classes and Data class

---
+++ source/Modules/LucasTechnologyServices.Module.ServiceDesk/Data/ServiceDeskCustomModelBuilder.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LucasTechnologyServices.Module.ServiceDesk.Data
+{
+    public class ServiceDeskCustomModelBuilder : ICustomModelBuilder
+    {
+        
+    }
+}
