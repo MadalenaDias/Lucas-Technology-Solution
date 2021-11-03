06dbbc1db9e616014126c0d93d9d15cf11246e68
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-11-03 23:17:11 +0000

    "fet(#601): adjusting Call entity into Service Desk microservice"

--- source/Modules/LucasTechnologyServices.Module.ServiceDesk/Models/Call.cs
+++ source/Modules/LucasTechnologyServices.Module.ServiceDesk/Models/Call.cs
@@ -1,20 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using LucasTechnologyService.Infrastructure.Models;
+using LucasTechnologyServices.Module.Core.Models;
+using System;
 
 namespace LucasTechnologyServices.Module.ServiceDesk.Models
 {
     public class Call : EntityBase
     {
         public string CallNumber { get; set; }
-        
+
         public CategoryCall CategoryCall { get; set; }
-        
+
         public Customer Customer { get; set; }
-        
+
         public DateTimeOffset OrigDate { get; set; }
-        
+
         public DateTimeOffset EndDate { get; set; }
 
         public string AttendName { get; set; }
@@ -24,18 +23,18 @@ namespace LucasTechnologyServices.Module.ServiceDesk.Models
         public string EmailDirect { get; set; }
 
         public string EmailForward { get; set; }
-        
-        
-        
-        
-        
-        
-        
-        
-            
-        
-        
-        
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
