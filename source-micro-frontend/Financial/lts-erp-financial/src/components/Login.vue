3530bc06193dcb5fe8c3e5aa12d6150663159a9d
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-11-09 02:00:06 +0000

    fet(#602): implementing login component

---
+++ source-micro-frontend/Financial/lts-erp-financial/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login">
+        <div class="model fade" id="login" tabindex="-1" role="dialog" aria-labelledby="loginTitle" aria-hidden="true"></div>
+        <div class="model-dialog model-dialog-centered" role="document">
+            <div class="model-content">
+            
+                <div class="model-body">
+                    <ul class="nav nav-fill nav-pills md-3" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="pills-home-tab" role=""
+                        </li>
+                    
+                    </ul>
+                </div>
+                
+                
+                </div>
+            </div>
+        
+        </div>
+    
+    
+    </div>
+
+
+</template>
