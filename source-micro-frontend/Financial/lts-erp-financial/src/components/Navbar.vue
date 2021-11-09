fcc6061a42f26968597666b0d35347535a78daaf
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-11-09 14:43:13 +0000

    fet(#602): implementing Navbar component

---
+++ source-micro-frontend/Financial/lts-erp-financial/src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <div class="hello">
+        <nav class="navbar custom-nav navbar-expand-lg navbar-llght bg-light">
+            <div class="container">
+                <router-link class="nav-brand" to="/">Financial</router-link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                /button>></button>
+            </div>div>
+        </nav>
+        
+    </div>
+
+</template>
